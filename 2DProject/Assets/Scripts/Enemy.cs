b4a6aa3da3c8602f15f7e9385d7799ddc0e13897
Nachtrind <Kerstin@Nachtrind.de>
2016-06-01 16:51:36 +0200

    Termin 1

---
+++ 2DProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	Rigidbody rig;
+
+	public float enemymovement;
+
+	// Use this for initialization
+	void Start () {
+	
+		rig = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Grounded() == true) {
+			rig.velocity = new Vector3 (enemymovement * Time.deltaTime, rig.velocity.y, 0);
+		} else {
+			enemymovement = enemymovement * -1;
+			rig.velocity = new Vector3 (enemymovement * Time.deltaTime, rig.velocity.y, 0);
+		}
+	}
+	void OnTriggerEnter (Collider c ){
+
+		if (c.gameObject.CompareTag ("Player")) {
+			Destroy (c.gameObject);
+	
+		}
+	}
+	bool Grounded(){
+		return Physics.Raycast (transform.position, Vector3.down, 0.7f);
+	}
+}
---
+++ 2DProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+	Rigidbody rig;
+	float move;
+	public float speed;
+	public float jump;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		rig = GetComponent<Rigidbody> ();
+
+ 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		move = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		rig.velocity = new Vector3 (move, rig.velocity.y, 0);
+
+		if (Input.GetKeyDown ("space")) {
+			if (Grounded () == true) {
+				rig.AddForce (new Vector3 (0, jump, 0));
+			}
+		}
+	}
+
+	bool IsGrounded ()
+	{
+
+		Vector3 pos = transform.position;
+		Vector3 end = pos + new Vector3 (0, -1.0f, 0);
+
+		if (Physics.Raycast (pos, new Vector3 (0, -1.0f, 0), 0.3f)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	bool Grounded(){
+		return Physics.Raycast(transform.position, Vector3.down, 0.7f);
+	}
+}
